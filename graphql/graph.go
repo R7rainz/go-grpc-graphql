10635352465a160cfd3432cd3c6089aca8ed6d1b
rainz <ronakkamboj26@gmail.com>
2026-07-29 17:15:21 +0530

    WIP wire GraphQL resolvers

--- graphql/graph.go
+++ graphql/graph.go
@@ -1,1 +1,61 @@
 package graphql
+
+import "github.com/99designs/gqlgen/graphql"
+
+//main graphql gateway, from here I will call all the clients
+
+type Server struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
+}
+
+func NewGraphQLServer(accountURL, catalogURL, orderURL string) (*Server, error) {
+	accountClient, err := account.NewClient(accountURL)
+	if err != nil {
+		return nil, err
+	}
+
+	catalogClient, err := catalog.NewClient(catalogURL)
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+
+	orderClient, err := order.NewClient(orderURL)
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return nil, err
+	}
+
+	return &Server{
+		accountClient,
+		catalogClient,
+		orderClient,
+	}, nil
+}
+
+func (s *Server) Mutation() mutationResolver {
+	return &mutationResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Query() QueryResolver {
+	return &queryResolver{
+		server: s,
+	}
+}
+
+func (s *Server) Account() AccountResolver {
+	return &accountResolver{
+		server: s,
+	}
+}
+
+func (s *Server) ToExecutableSchema() graphql.ExecutableSchema {
+	return NewExecutableSchema(Config{
+		Resolvers: s,
+	})
+}
